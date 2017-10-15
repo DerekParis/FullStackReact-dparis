cc2160ddd7727915a4caecd2f3bffd414bc9f3be
Derek Paris <derekparis@email.arizona.edu>
2017-10-15 14:39:07 -0700

    heroku pre deployment finished

--- server/index.js
+++ server/index.js
@@ -1,2 +1,16 @@
-const express = require("express"); //imports in required express library
+const express = require('express'); //imports in the required express library
 const app = express(); //generates a new, running express app
+
+//prints the object upon visiting localhost:5000/
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+//prints the object upon visiting localhost:5000/greeting
+app.get('/greeting', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+//dynamic port binding from heroku
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
