ac81dd0fed656259cb4e6c178d263cc86075bcad
Derek Paris <derekparis@email.arizona.edu>
2017-10-16 15:23:37 -0700

    google oauth section completed

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 const express = require('express'); //imports in the required express library
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express(); //generates a new, running express app
+
+//important features to use passport
+passport.use(
+  //GoogleStrategy is specific to google+ login. Is named 'google'
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    //what do after receiving a callback
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken);
+      console.log('refresh token', refreshToken);
+      console.log('profile:', profile);
+    }
+  )
+);
+
+app.get(
+  '/auth/google', //our route
+  //use 'google' to access GoogleStrategy
+  passport.authenticate('google', {
+    scope: ['profile', 'email'] //specific scopes that are names within google's oauth
+  })
+);
+
+//for passport to handle the account upon callback
+//passport will see that the user code=xxxxx is within the URL
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 //prints the object upon visiting localhost:5000/
 app.get('/', (req, res) => {
